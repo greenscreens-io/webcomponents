75215374354398a9966f411ff37bd49bf433a927
Green Screens Ltd <greenscreens.ltd@gmail.com>
2022-08-20 15:40:51 +0200

    GSNotification improvements

--- modules/components/toast/GSNotification.mjs
+++ modules/components/toast/GSNotification.mjs
@@ -17,23 +17,64 @@ import GSUtil from "../../base/GSUtil.mjs";
  */
 export default class GSNotification extends GSElement {
 
+  static TOP_START = "position-fixed top-0 start-0";
+  static TOP_CENTER ="position-fixed top-0 start-50 translate-middle-x";
+  static TOP_END = "position-fixed top-0 end-0";
+  static MIDDLE_START = "position-fixed top-50 start-0 translate-middle-y";
+  static MIDDLE_CENTER = "position-fixed top-50 start-50 translate-middle";
+  static MIDDLE_END = "position-fixed top-50 end-0 translate-middle-y";
+  static BOTTOM_START = "position-fixed bottom-0 start-0";
+  static BOTTOM_CENTER ="position-fixed bottom-0 start-50 translate-middle-x";
+  static BOTTOM_END = "position-fixed bottom-0 end-0";
+
+  static DEFAULT = GSNotification.BOTTOM_END;
+
   static {
     customElements.define('gs-notification', GSNotification);
   }
 
+  static get observedAttributes() {
+    const attrs = ['position'];
+    return GSUtil.mergeArrays(attrs, super.observedAttributes);
+  }
+
+  attributeCallback(name = '', oldVal = '', newVal = '') {
+    const me = this;
+
+    oldVal = me.#fromPosition(oldVal);
+    newVal = me.#fromPosition(newVal);
+
+    const wrapEl = me.findEl('div');
+    GSUtil.toggleClass(wrapEl, false, oldVal);
+    GSUtil.toggleClass(wrapEl, true, newVal);
+  }
+
+  #fromPosition(val) {
+    let css = GSNotification[val];
+    return GSUtil.isString(css) && css.startsWith('position-') ? css : val;
+  }
+
   async getTemplate(val = '') {
     const me = this;
-    return `<div class="toast-container ${me.css}" style="z-index: 10000;">
+    return `<div class="toast-container ${me.css} ${me.position}" style="z-index: 10000;">
       <slot name="content"></slot>
     </div>`;
   }
 
   get css() {
-    return GSUtil.getAttribute(this, 'css', 'position-fixed bottom-0 end-0 p-3');
+    return GSUtil.getAttribute(this, 'css', 'p-3');
   }
 
   set css(val = '') {
     GSUtil.setAttribute(this, 'css', val);
+  }
+
+  get position() {
+    return GSUtil.getAttribute(this, 'position', GSNotification.DEFAULT);
+  }
+
+  set position(val = '') {
+    GSUtil.setAttribute(this, 'position', val);
   }
 
   info(title = '', message = '', closable = false, timeout = 2) {
