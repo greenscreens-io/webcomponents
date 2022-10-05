aa5a2f751608be4b88ff25037eeb84b45edd699e
Green Screens Ltd <greenscreens.ltd@gmail.com>
2022-10-05 20:41:19 +0200

    WebAdmin demo updates

---
+++ demos/webadmin/scripts/views/filters/GSFilterFido.mjs
@@ -0,0 +1,22 @@
+/*
+* Copyright (C) 2015, 2022 Green Screens Ltd.
+*/
+
+/**
+ * A module loading GSFilterFido class
+ * @module views/GSFilterFido
+ */
+ import BaseViewUI from '../BaseViewUI.mjs';
+
+ export default class GSFilterFido extends BaseViewUI {
+ 
+     static {
+         customElements.define('gs-admin-view-filterfido', GSFilterFido);
+         Object.seal(GSFilterFido);
+     }
+ 
+     async getTemplate() {
+        return super.getTemplate('//views/filter-fido.html');
+    }
+
+ }
