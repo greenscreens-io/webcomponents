4200a746ac76d36962da8d348bd45b75c722ed44
Green Screens Ltd <greenscreens.ltd@gmail.com>
2022-11-14 15:56:07 +0100

    GSContext, GSDropdown default self action trigger

--- modules/components/GSDropdown.mjs
+++ modules/components/GSDropdown.mjs
@@ -14,6 +14,7 @@ import GSLoader from "../base/GSLoader.mjs";
 import GSUtil from "../base/GSUtil.mjs";
 import GSAttr from "../base/GSAttr.mjs";
 import GSDOM from "../base/GSDOM.mjs";
+import GSFunction from "../base/GSFunction.mjs";
 
 /**
  * Dropdown menu
