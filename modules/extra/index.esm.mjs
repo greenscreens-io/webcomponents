a92d162f1858f7b54328f7b205a7cbba34772ffa
Green Screens Ltd <greenscreens.ltd@gmail.com>
2023-05-23 11:34:22 +0200

    Added GSMarkdown

--- modules/extra/index.esm.mjs
+++ modules/extra/index.esm.mjs
@@ -1,7 +1,8 @@
 /*
- * Copyright (C) 2015, 2022 Green Screens Ltd.
+ * Copyright (C) 2015, 2023 Green Screens Ltd.
  */
 
 export { default as GSChart } from './GSChart.mjs';
 export { default as GSHighlight } from './GSHighlight.mjs';
 export { default as GSMonaco } from './GSMonaco.mjs';
+export { default as GSMarkdown } from './GSMarkdown.mjs';
