e5d942e93dc2341c24e660f0c9862b67a918092f
Green Screens Ltd <greenscreens.ltd@gmail.com>
2023-09-07 11:28:36 +0200

    Client revoke

--- demos/webadmin/modules/dialogs/GSCertRevoke.mjs
+++ demos/webadmin/modules/dialogs/GSCertRevoke.mjs
@@ -29,40 +29,11 @@ export default class GSCertRevoke extends GSAsbtractDialog {
         return 'Revoke Client Certificate';
     }
 
-    get input () {
-        return GSDOM.query(this, 'gs-filebox')?.query('input');
-    }
-
-    get file() {
-        const input = this.input;
-        if (!input?.files?.length > 0) return false;
-        return input.files[0];
-    }
-
-    get isPEM() {
-		return this.isExt('.pem') || this.isExt('.cer');
-	}
-
-    get isPFX() {
-		return this.isExt('.pfx');
-	}
-
-    isExt(val) {
-		return this.file?.name.endsWith(val);
-	}
-	    	
-    async fileToHex() {
-        const buff = await this.file?.arrayBuffer();
-        return Utils.toHex(buff);
-    }
-
     async onData(data) {
 		if (DEMO) return DEMO.success;
 		const me = this;
-		let tmp = data.email;
-		if (!tmp) tmp = await me.fileToHex();
-		if (!tmp) return false;
-	    const o = await io.greenscreens.Certificate.revokeClient(tmp);
+		if (!data?.email) return false;
+	    const o = await io.greenscreens.Certificate.revokeClient(data.email);
 	    if (o.msg) Utils.notify.secondary('Client certificate revoking', o.msg, true, 4, 0.2);
 	    me.enable();
 	    me.visible = false;
