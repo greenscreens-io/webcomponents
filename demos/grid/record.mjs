321e5a7c1a92161f7a4880bbc94e13089b58f622
Green Screens Ltd. <greenscreens.ltd@gmail.com>
2025-01-20 12:16:49 +0100

    Upgrade to v2.x

---
+++ demos/grid/record.mjs
@@ -0,0 +1,110 @@
+
+import { GSData } from '../../modules/base/GSData.mjs';
+import { GSEvents } from '../../modules/base/GSEvents.mjs';
+import { GSUtil } from '../../modules/base/GSUtil.mjs';
+import { GSDialogElement } from "../../modules/components/Dialog.mjs";
+import { GSReadWriteRegistry } from "../../modules/data/ReadWriteRegistry.mjs";
+
+export class RecordDialog extends GSDialogElement {
+
+    static {
+        this.define('gs-record-edit');
+    }
+
+    #data;
+
+    connectedCallback() {
+        super.connectedCallback();
+        const me = this;
+        me.opened = true;
+        me.closable = true;
+        me.cancelable = true;
+        me.escapable = true;
+        me.cssBody = 'p-0';
+        me.cssHeader = 'p-3 dialog-title';              
+        me.title = "Edit record";
+        me.template = "//record.html";
+    }
+
+    firstUpdated() {
+        super.firstUpdated();
+        const me = this;
+        me.on('data', me.#onFormData.bind(me));
+        me.on('error', me.#onFormError.bind(me));
+        me.on('notify', me.#onNotify.bind(me));
+        GSEvents.monitorAction(this);
+    }
+
+    // override sper calss function after dialog tempalte (form) is injecetd
+    // we use this to load data into a form
+    async templateInjected() {
+        return this.onLoad();
+    }
+
+    // TODO save form data on OK click
+    async onData(data) {
+        //this.#data = GSData.mergeObjects([this.#data, data]);
+        Object.assign(this.#data, data);
+        alert(JSON.stringify(this.#data));
+        this.table?.requestUpdate();
+        return true;
+    }
+
+    /**
+     * Load data into a form from shared storage engien
+     * by retrieveing first grid selected record.
+     * @returns 
+     */
+    async onLoad() {
+        const storage = GSReadWriteRegistry.find('employees');
+        const form = this.form;
+        if (form && storage) {            
+            form.data = this.#data = storage?.selected.pop();
+        }
+    }    
+
+    get form() {
+        return this.query('gs-form', true);
+    }
+ 
+    get table() {
+        return GSDOM.query('gs-table');
+    }
+
+    // auto remove on close
+    #onNotify(e) {
+        if (!this.opened) this.remove();
+    }
+
+    #onFormError(e) {
+        alert('Some fields are invalid!');
+    }
+
+    #onFormData(e) {
+        // prevent close on confirm click
+        GSEvents.prevent(e);
+        this.#handleFormData(e);
+    }
+
+    async #handleFormData(e) {
+        const me = this;
+        let sts = false;
+        try {
+            me.disabled = true;
+            sts = await me.onData(e.detail);
+        } catch (e) {
+            me.onError(e);
+        } finally {
+            me.disabled = false;
+            if (sts) {
+                me.close();
+            }
+        }
+    }
+
+    onError(e) {
+        console.log(e);
+        const msg = e.data?.error || e.msg || e.message || e.toString();
+        alert(msg);
+    }    
+}
