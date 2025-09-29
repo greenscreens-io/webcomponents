bc21f2c952401181f5725641f0bd92d8bb0f557e
Green Screens Ltd. <greenscreens.ltd@gmail.com>
2025-09-29 10:22:54 +0200

    Update to 2.1.x version

---
+++ demos/webadmin/modules/dialogs/GSSyslog.mjs
@@ -0,0 +1,36 @@
+/*
+* Copyright (C) 2015, 2025 Green Screens Ltd.
+*/
+
+import { GSAsbtractDialog } from './GSAsbtractDialog.mjs';
+
+/**
+ * A module loading GSOtpOptions class
+ * @module dialogs/GSOtpOptions
+ */
+export class GSSyslog extends GSAsbtractDialog {
+
+    static {
+        this.define('gs-admin-dialog-syslog');
+    }
+
+    constructor() {
+        super();
+        const me = this;
+        me.opened = true;
+        me.dismissable = true;
+        me.title = "Syslog Options";
+        me.template = "//dialogs/syslog.html";
+    }
+
+    async onData(data) {
+        const o = DEMO ? DEMO : await io.greenscreens.Server.setSyslog(data);
+        return o.success;
+    }
+
+    async loadDefaults() {
+        const o = DEMO ? DEMO : await io.greenscreens.Server.getSyslog();
+        return o.data;
+    }
+
+}
