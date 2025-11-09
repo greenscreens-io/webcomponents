946bb924a1cb7b2b0111eb60d37a8418cf407100
Green Screens Ltd. <greenscreens.ltd@gmail.com>
2025-11-09 13:09:07 +0100

    Webadmin demo updates

---
+++ demos/webadmin/modules/dialogs/GSKerblite.mjs
@@ -0,0 +1,36 @@
+/*
+* Copyright (C) 2015, 2025 Green Screens Ltd.
+*/
+
+import { GSAsbtractDialog } from './GSAsbtractDialog.mjs';
+
+/**
+ * A module loading GSKerbLite class
+ * @module dialogs/GSKerbLite
+ */
+export class GSKerbLite extends GSAsbtractDialog {
+
+    static {
+        this.define('gs-admin-dialog-kerblite');
+    }
+
+    constructor() {
+        super();
+        const me = this;
+        me.opened = true;
+        me.dismissable = true;
+        me.title = "SSO Lite";
+        me.template = "//dialogs/kerblite.html";
+    }
+
+    async onData(data) {
+        const o = DEMO ? DEMO : await io.greenscreens.Kerberos.saveLite(data);
+        return o.success;
+    }
+
+    async loadDefaults() {
+        const o = DEMO ? DEMO : await io.greenscreens.Kerberos.loadLite();
+        return o.data;
+    }
+    
+}
